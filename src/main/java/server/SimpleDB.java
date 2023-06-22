7c702368a19116261a6b81ffd7028d16a60d4861
matsu-chara <matsuy00@gmail.com>
2023-06-23 07:19:28 +0900

    adjust SimpleDB class

--- src/main/java/server/SimpleDB.java
+++ src/main/java/server/SimpleDB.java
@@ -1,17 +1,23 @@
 package server;
 
 import file.FileMgr;
+import log.LogMgr;
+
 import java.io.File;
 
 public class SimpleDB {
   public static int BLOCK_SIZE = 400;
   public static int BUFFER_SIZE = 8;
 
+  public static String LOG_FILE = "simpledb.log";
+
   private FileMgr fm;
+  private LogMgr lm;
 
   public SimpleDB(String dirname, int blocksize, int buffsize) {
     File dbDirectory = new File(dirname);
     fm = new FileMgr(dbDirectory, blocksize);
+    lm = new LogMgr(fm, LOG_FILE);
   }
 
   public SimpleDB(String dirname) {
