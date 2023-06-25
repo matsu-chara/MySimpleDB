d3acf412228d71a94ed4cd0740def7d6e97cf5bb
matsu-chara <matsuy00@gmail.com>
2023-06-26 07:11:59 +0900

    fmt

--- src/main/java/tx/concurrency/ConcurrencyMgr.java
+++ src/main/java/tx/concurrency/ConcurrencyMgr.java
@@ -1,40 +1,38 @@
 package tx.concurrency;
 
-
 import file.BlockId;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class ConcurrencyMgr {
-    private static LockTable lockTbl = new LockTable();
+  private static LockTable lockTbl = new LockTable();
 
-    private Map<BlockId, String> locks = new HashMap<>();
+  private Map<BlockId, String> locks = new HashMap<>();
 
-    public void slock(BlockId blk) {
-        if (locks.get(blk) == null) {
-            lockTbl.slock(blk);
-            locks.put(blk, "S");
-        }
+  public void slock(BlockId blk) {
+    if (locks.get(blk) == null) {
+      lockTbl.slock(blk);
+      locks.put(blk, "S");
     }
+  }
 
-    public void xlock(BlockId blk) {
-        if(!hasXlock(blk)) {
-            slock(blk); // shared lockをとってからexclusive lockにupgradeする
-            lockTbl.xlock(blk);
-            locks.put(blk, "X");
-        }
+  public void xlock(BlockId blk) {
+    if (!hasXlock(blk)) {
+      slock(blk); // shared lockをとってからexclusive lockにupgradeする
+      lockTbl.xlock(blk);
+      locks.put(blk, "X");
     }
+  }
 
-    public void release() {
-        for(BlockId blk: locks.keySet()) {
-            lockTbl.unlock(blk);
-        }
-        locks.clear();
+  public void release() {
+    for (BlockId blk : locks.keySet()) {
+      lockTbl.unlock(blk);
     }
+    locks.clear();
+  }
 
-    private boolean hasXlock(BlockId blk) {
-        String lockType = locks.get(blk);
-        return lockType != null && lockType.equals("X");
-    }
+  private boolean hasXlock(BlockId blk) {
+    String lockType = locks.get(blk);
+    return lockType != null && lockType.equals("X");
+  }
 }
