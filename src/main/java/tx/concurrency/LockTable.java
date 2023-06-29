dcb5578b9e70a97c9c68ad299d8ea5fc67d0b0b6
matsu-chara <matsuy00@gmail.com>
2023-06-30 08:08:22 +0900

    fmt

--- src/main/java/tx/concurrency/LockTable.java
+++ src/main/java/tx/concurrency/LockTable.java
@@ -1,81 +1,78 @@
 package tx.concurrency;
 
 import file.BlockId;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class LockTable {
-    private static int DEFAULT_MAX_TIME = 10000; // 10sec
-    private long maxTime;
+  private static int DEFAULT_MAX_TIME = 10000; // 10sec
+  private long maxTime;
 
-    private Map<BlockId, Integer> locks = new HashMap<>();
+  private Map<BlockId, Integer> locks = new HashMap<>();
 
-    public LockTable() {
-        this.maxTime = DEFAULT_MAX_TIME;
+  public LockTable() {
+    this.maxTime = DEFAULT_MAX_TIME;
+  }
+
+  public LockTable(long maxTime) {
+    this.maxTime = maxTime;
+  }
+
+  synchronized void slock(BlockId blk) {
+    try {
+      long starttime = System.currentTimeMillis();
+      while (hasXLock(blk) && !waitingTooLong(starttime)) {
+        wait(maxTime);
+      }
+      if (hasXLock(blk)) {
+        throw new LockAbortException();
+      }
+      int val = getLockVal(blk); // will not be negative
+      locks.put(blk, val + 1);
+    } catch (InterruptedException e) {
+      throw new LockAbortException();
     }
+  }
 
-    public LockTable(long maxTime) {
-        this.maxTime = maxTime;
+  synchronized void xlock(BlockId blk) {
+    try {
+      long starttime = System.currentTimeMillis();
+      while (hasOtherSLocks(blk) && !waitingTooLong(starttime)) {
+        wait(maxTime);
+      }
+      if (hasOtherSLocks(blk)) {
+        throw new LockAbortException();
+      }
+      locks.put(blk, -1);
+    } catch (InterruptedException e) {
+      throw new LockAbortException();
     }
+  }
 
-    synchronized void slock(BlockId blk) {
-        try {
-            long starttime = System.currentTimeMillis();
-            while(hasXLock(blk) && !waitingTooLong(starttime)) {
-                wait(maxTime);
-            }
-            if(hasXLock(blk)) {
-                throw new LockAbortException();
-            }
-            int val = getLockVal(blk);  // will not be negative
-            locks.put(blk, val + 1);
-        } catch (InterruptedException e) {
-            throw new LockAbortException();
-        }
+  synchronized void unlock(BlockId blk) {
+    int val = getLockVal(blk);
+    if (val > 1) {
+      locks.put(blk, val - 1);
+    } else {
+      locks.remove(blk);
+      notifyAll();
     }
+  }
 
-    synchronized void xlock(BlockId blk) {
-        try {
-            long starttime = System.currentTimeMillis();
-            while(hasOtherSLocks(blk) && !waitingTooLong(starttime)) {
-                wait(maxTime);
-            }
-            if(hasOtherSLocks(blk)) {
-                throw new LockAbortException();
-            }
-            locks.put(blk, -1);
-        } catch (InterruptedException e) {
-            throw new LockAbortException();
-        }
-    }
+  private boolean hasXLock(BlockId blk) {
+    return getLockVal(blk) < 0; // マイナスならexclusive lock
+  }
 
-    synchronized void unlock(BlockId blk) {
-        int val = getLockVal(blk);
-        if (val > 1){
-            locks.put(blk, val - 1);
-        } else {
-            locks.remove(blk);
-            notifyAll();
-        }
+  private boolean hasOtherSLocks(BlockId blk) {
+    return getLockVal(blk) > 1;
+  }
 
-    }
+  private boolean waitingTooLong(long starttime) {
+    return System.currentTimeMillis() - starttime > maxTime;
+  }
 
-    private boolean hasXLock(BlockId blk) {
-        return getLockVal(blk) < 0; // マイナスならexclusive lock
-    }
-
-
-    private boolean hasOtherSLocks(BlockId blk) {
-        return getLockVal(blk) > 1;
-    }
-
-    private boolean waitingTooLong(long starttime) {
-        return System.currentTimeMillis() - starttime > maxTime;
-    }
-
-    private int getLockVal(BlockId blk) {
-        Integer ival = locks.get(blk);
-        return (ival == null)? 0 : ival;
-    }
+  private int getLockVal(BlockId blk) {
+    Integer ival = locks.get(blk);
+    return (ival == null) ? 0 : ival;
+  }
 }
