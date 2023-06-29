29807daedee2ee90fc1519c41d410355e224bbf6
matsu-chara <matsuy00@gmail.com>
2023-06-29 09:32:28 +0900

    add transaction

--- src/main/java/tx/Transaction.java
+++ src/main/java/tx/Transaction.java
@@ -1,49 +1,116 @@
 package tx;
 
+import buffer.Buffer;
 import buffer.BufferMgr;
 import file.BlockId;
 import file.FileMgr;
 import log.LogMgr;
+import tx.concurrency.ConcurrencyMgr;
 import tx.recovery.RecoveryMgr;
 
 public class Transaction {
   private FileMgr fm;
-  private LogMgr lm;
   private BufferMgr bm;
 
   private int txnum;
   private static int nextTxNum = 0;
+  private static int END_OF_FILE = -1;
+
   private RecoveryMgr recoveryMgr;
+  private ConcurrencyMgr concurMgr;
+
+  private BufferList mybuffers;
 
   public Transaction(FileMgr fm, LogMgr lm, BufferMgr bm) {
     this.fm = fm;
-    this.lm = lm;
     this.bm = bm;
     this.txnum = nextTxNumber();
     this.recoveryMgr = new RecoveryMgr(lm, bm, this, txnum);
+    this.concurMgr = new ConcurrencyMgr();
+    this.mybuffers = new BufferList(bm);
   }
 
   public void commit() {
     recoveryMgr.commit();
-    System.out.println("Transaction: " + txnum + " committed.");
+    concurMgr.release();
+    mybuffers.unpinAll();
+    System.out.println("Transaction: " + txnum + " committed."); // log目的で残しておく
   }
 
   public void rollback() {
     recoveryMgr.rollback();
-    System.out.println("Transaction: " + txnum + " rollback.");
+    concurMgr.release();
+    mybuffers.unpinAll();
+    System.out.println("Transaction: " + txnum + " rollback."); // log目的で残しておく
   }
 
   public void recover() {
+    bm.flushAll(
+        txnum); // recoveryMgrもflushするので不要といえば不要。(bufferPool全体をflushしてるけどmybuffersをflushするようにしたらちょっとだけ性能上がりそう）
     recoveryMgr.recover();
   }
 
-  public void pin(BlockId blk) {}
+  public void pin(BlockId blk) {
+    mybuffers.pin(blk);
+  }
 
-  public void unpin(BlockId blk) {}
+  public void unpin(BlockId blk) {
+    mybuffers.unpin(blk);
+  }
 
-  public void setInt(BlockId blk, int offset, int val, boolean okToLog) {}
+  public int getInt(BlockId blk, int offset) {
+    concurMgr.slock(blk);
+    Buffer buff = mybuffers.getBuffer(blk);
+    return buff.contents().getInt(offset);
+  }
 
-  public void setString(BlockId blk, int offset, String val, boolean okToLog) {}
+  public String getString(BlockId blk, int offset) {
+    concurMgr.slock(blk);
+    Buffer buff = mybuffers.getBuffer(blk);
+    return buff.contents().getString(offset);
+  }
+
+  public void setInt(BlockId blk, int offset, int val, boolean okToLog) {
+    concurMgr.xlock(blk);
+    Buffer buff = mybuffers.getBuffer(blk);
+    int lsn = -1;
+    if (okToLog) {
+      lsn = recoveryMgr.setInt(buff, offset);
+    }
+    buff.contents().setInt(offset, val);
+    buff.setModified(txnum, lsn);
+  }
+
+  public void setString(BlockId blk, int offset, String val, boolean okToLog) {
+    concurMgr.xlock(blk);
+    Buffer buff = mybuffers.getBuffer(blk);
+    int lsn = -1;
+    if (okToLog) {
+      lsn = recoveryMgr.setString(buff, offset);
+    }
+    buff.contents().setString(offset, val);
+    buff.setModified(txnum, lsn);
+  }
+
+  public int size(String filename) {
+    BlockId dummyblk = new BlockId(filename, END_OF_FILE);
+    concurMgr.slock(dummyblk);
+    return fm.length(filename);
+  }
+
+  public BlockId append(String filename) {
+    BlockId dummyblk = new BlockId(filename, END_OF_FILE);
+    concurMgr.slock(dummyblk);
+    return fm.append(filename);
+  }
+
+  public int blockSize() {
+    return fm.blockSize();
+  }
+
+  public int availableBuffers() {
+    return bm.available();
+  }
 
   private static synchronized int nextTxNumber() {
     nextTxNum++;
--- src/main/java/tx/recovery/RecoveryMgr.java
+++ src/main/java/tx/recovery/RecoveryMgr.java
@@ -1,92 +1,96 @@
 package tx.recovery;
 
+import buffer.Buffer;
+import buffer.BufferMgr;
+import file.BlockId;
+import log.LogMgr;
+import tx.Transaction;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
 import static tx.recovery.LogRecord.CHECKPOINT;
 import static tx.recovery.LogRecord.COMMIT;
 import static tx.recovery.LogRecord.ROLLBACK;
 import static tx.recovery.LogRecord.START;
 
-import buffer.Buffer;
-import buffer.BufferMgr;
-import file.BlockId;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import log.LogMgr;
-import tx.Transaction;
-
 public class RecoveryMgr {
-  private LogMgr lm;
-  private BufferMgr bm;
-  private Transaction tx;
-  private int txnum;
+    private LogMgr lm;
+    private BufferMgr bm;
+    private Transaction tx;
+    private int txnum;
 
-  public RecoveryMgr(LogMgr lm, BufferMgr bm, Transaction tx, int txnum) {
-    this.lm = lm;
-    this.bm = bm;
-    this.tx = tx;
-    this.txnum = txnum;
-    StartRecord.writeToLog(lm, txnum);
-  }
+    public RecoveryMgr(LogMgr lm, BufferMgr bm, Transaction tx, int txnum) {
+        this.lm = lm;
+        this.bm = bm;
+        this.tx = tx;
+        this.txnum = txnum;
+        StartRecord.writeToLog(lm, txnum);
+    }
 
-  public void commit() {
-    bm.flushAll(txnum); // log書き込み => pageをblockにflush => commitRecordをログにflush
-    int lsn = CommitRecord.writeToLog(lm, txnum);
-    lm.flush(lsn);
-  }
+    public void commit() {
+        bm.flushAll(txnum); // log書き込み => pageをblockにflush => commitRecordをログにflush
+        int lsn = CommitRecord.writeToLog(lm, txnum);
+        lm.flush(lsn);
+    }
 
-  public void rollback() {
-    doRollBack();
-    bm.flushAll(txnum);
-    int lsn = RollbackRecord.writeToLog(lm, txnum);
-    lm.flush(lsn);
-  }
+    public void rollback() {
+        doRollBack();
+        bm.flushAll(txnum);
+        int lsn = RollbackRecord.writeToLog(lm, txnum);
+        lm.flush(lsn);
+    }
 
-  public void recover() {
-    doRecover();
-    bm.flushAll(txnum);
-    int lsn = CheckpointRecord.writeToLog(lm);
-    lm.flush(lsn);
-  }
+    public void recover() {
+        doRecover();
+        bm.flushAll(txnum);
+        int lsn = CheckpointRecord.writeToLog(lm);
+        lm.flush(lsn);
+    }
 
-  public int setInt(Buffer buff, int offset) {
-    int oldval = buff.contents().getInt(offset);
-    BlockId blk = buff.block();
-    return SetIntRecord.writeToLog(lm, txnum, blk, offset, oldval);
-  }
+    // newvalは今回の実装だと利用してないのでサンプルコードにはあるけど消してみた
+    // ただsetIntとsetStringを正確に呼び分けないとランタイムエラーになるので悪手だだったかもsrc/main/java/tx/recovery/RecoveryMgr.java
+    
+    public int setInt(Buffer buff, int offset) {
+        int oldval = buff.contents().getInt(offset);
+        BlockId blk = buff.block();
+        return SetIntRecord.writeToLog(lm, txnum, blk, offset, oldval);
+    }
 
-  public int setString(Buffer buff, int offset) {
-    String oldval = buff.contents().getString(offset);
-    BlockId blk = buff.block();
-    return SetStringRecord.writeToLog(lm, txnum, blk, offset, oldval);
-  }
+    public int setString(Buffer buff, int offset) {
+        String oldval = buff.contents().getString(offset);
+        BlockId blk = buff.block();
+        return SetStringRecord.writeToLog(lm, txnum, blk, offset, oldval);
+    }
 
-  private void doRollBack() {
-    Iterator<byte[]> iter = lm.iterator();
-    while (iter.hasNext()) {
-      byte[] bytes = iter.next();
-      LogRecord rec = LogRecord.createLogRecord(bytes);
-      if (rec.txNumber() == txnum) { // いま考えてるtxのレコードのみをrollbackする
-        if (rec.op() == START) { // beginを見つけたら終わり
-          return;
+    private void doRollBack() {
+        Iterator<byte[]> iter = lm.iterator();
+        while(iter.hasNext()) {
+            byte[] bytes = iter.next();
+            LogRecord rec = LogRecord.createLogRecord(bytes);
+            if(rec.txNumber() == txnum) { // いま考えてるtxのレコードのみをrollbackする
+                if(rec.op() == START) { // beginを見つけたら終わり
+                    return;
+                }
+                rec.undo(tx);
+            }
         }
-        rec.undo(tx);
-      }
     }
-  }
 
-  private void doRecover() {
-    Collection<Integer> finishedTxs = new ArrayList<>();
-    Iterator<byte[]> iter = lm.iterator();
-    while (iter.hasNext()) {
-      byte[] bytes = iter.next();
-      LogRecord rec = LogRecord.createLogRecord(bytes);
-      if (rec.op() == CHECKPOINT) {
-        return; // CHECKPOINTレコードかlogの終端まで読み切ったらおわり
-      } else if (rec.op() == COMMIT || rec.op() == ROLLBACK) {
-        finishedTxs.add(rec.txNumber()); // commit or rollbackレコードがあるならこのトランザクションをundoする必要はない
-      } else if (!finishedTxs.contains(rec.txNumber())) {
-        rec.undo(tx);
-      }
+    private void doRecover() {
+        Collection<Integer> finishedTxs = new ArrayList<>();
+        Iterator<byte[]> iter = lm.iterator();
+        while(iter.hasNext()){
+            byte[] bytes = iter.next();
+            LogRecord rec = LogRecord.createLogRecord(bytes);
+            if(rec.op() == CHECKPOINT) {
+                return; // CHECKPOINTレコードかlogの終端まで読み切ったらおわり
+            } else if(rec.op() == COMMIT || rec.op() == ROLLBACK) {
+                finishedTxs.add(rec.txNumber()); // commit or rollbackレコードがあるならこのトランザクションをundoする必要はない
+            } else if(!finishedTxs.contains(rec.txNumber())) {
+                rec.undo(tx);
+            }
+        }
     }
-  }
 }
