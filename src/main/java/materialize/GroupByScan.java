114f2e614ccebd73efb667cd6fac46c52c0fc2af
matsu-chara <matsuy00@gmail.com>
2023-09-02 08:49:57 +0900

    groupby scan

---
+++ src/main/java/materialize/GroupByScan.java
@@ -0,0 +1,90 @@
+package materialize;
+
+import java.util.List;
+import query.Constant;
+import query.Scan;
+
+public class GroupByScan implements Scan {
+  private Scan s;
+  private List<String> groupfields;
+  private List<AggregationFn> aggfns;
+  private GroupValue groupval;
+  private boolean moregroups;
+
+  public GroupByScan(Scan s, List<String> groupfields, List<AggregationFn> aggfns) {
+    this.s = s;
+    this.groupfields = groupfields;
+    this.aggfns = aggfns;
+    beforeFirst();
+  }
+
+  @Override
+  public void beforeFirst() {
+    s.beforeFirst();
+    moregroups = s.next();
+  }
+
+  @Override
+  public boolean next() {
+    if (!moregroups) {
+      return false;
+    }
+    for (var fn : aggfns) {
+      fn.processFirst(s);
+    }
+    groupval = new GroupValue(s, groupfields);
+
+    while (moregroups) {
+      moregroups = s.next();
+      var gv = new GroupValue(s, groupfields);
+      if (!groupval.equals(gv)) {
+        break;
+      }
+      for (var fn : aggfns) {
+        fn.processNext(s);
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public int getInt(String fldname) {
+    return getVal(fldname).asInt();
+  }
+
+  @Override
+  public String getString(String fldname) {
+    return getVal(fldname).asString();
+  }
+
+  @Override
+  public Constant getVal(String fldname) {
+    if (groupfields.contains(fldname)) {
+      return groupval.getVal(fldname);
+    }
+    for (var fn : aggfns) {
+      if (fn.fieldName().equals(fldname)) {
+        return fn.value();
+      }
+    }
+    throw new RuntimeException("field " + fldname + " not found.");
+  }
+
+  @Override
+  public boolean hasField(String fldname) {
+    if (groupfields.contains(fldname)) {
+      return true;
+    }
+    for (var fn : aggfns) {
+      if (fn.fieldName().equals(fldname)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public void close() {
+    s.close();
+  }
+}
---
+++ src/main/java/materialize/GroupValue.java
@@ -0,0 +1,45 @@
+package materialize;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import query.Constant;
+import query.Scan;
+
+public class GroupValue {
+  private Map<String, Constant> vals = new HashMap<>();
+
+  public GroupValue(Scan s, List<String> fields) {
+    for (var fldname : fields) {
+      vals.put(fldname, s.getVal(fldname));
+    }
+  }
+
+  public Constant getVal(String fldname) {
+    return vals.get(fldname);
+  }
+
+  public boolean equals(Object obj) {
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+
+    var gv = (GroupValue) obj;
+    for (var fldname : vals.keySet()) {
+      var c1 = vals.get(fldname);
+      var c2 = gv.getVal(fldname);
+      if (!c1.equals(c2)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public int hashCode() {
+    var hashval = 0;
+    for (var c : vals.values()) {
+      hashval += c.hashCode();
+    }
+    return hashval;
+  }
+}
